4facfd1deac22592b82973c5ac39c88f047affad
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2024-09-30 09:40:39 -0400

    feature : Title of detail page changing with hero name

--- detail.php
+++ detail.php
@@ -6,19 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="/public/images/favicon.ico" type="image/x-icon">
-
-    <!-- NOTE: Title is different for this page -->
-    <title>Dota 2 - Anti-Mage</title>
-
-    <!-- TODO: Remove CDN link and include Bootstrap files locally -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="/public/stylesheets/styles.css?v=<?php echo time(); ?>">
-
     <?php
     $url = $_SERVER['REQUEST_URI'];
     $lastPart = strtolower(basename($url));
@@ -69,6 +56,20 @@
         }
     }
     ?>
+
+    <!-- NOTE: Title is different for this page -->
+    <title>Dota 2 - <? echo$lastPart ?></title>
+
+    <!-- TODO: Remove CDN link and include Bootstrap files locally -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="/public/stylesheets/styles.css?v=<?php echo time(); ?>">
+
+    
 
 </head>
 
